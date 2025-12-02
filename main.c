d9517cd4e00b5014206664f02121005fdbd970c7
robert <robert@none.com>
2025-12-02 16:39:20 +0000

    Completed main code

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 
 #include "raylib.h"
 #include <stdio.h>
-#include <math.h>
+#include "raymath.h"
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,18 +9,20 @@
 #define WIDTH 1200
 #define HEIGHT 700
 #define FPS 60
-#define CAM_ROTATE_SPEED 0.5f
+#define CAM_ROTATE_SPEED 0.7f
 
-#define NUM_OBJECTS 200
-#define SCENE_NUM 2
+#define MAX_OBJECTS 50
 
-#define G 6.6743*pow(10, -12)
-#define VEL_SCALE 0.00001
+#define G 6.6743*pow(10, -11)
+#define VEL_CHANGE 0.000001
+float vel_scale = 0.00001;
 
 #define BACKGROUND BLACK
 #define FOREGROUND WHITE
+#define TEXT_SCALE 20
+#define TEXT_COLOUR DARKGREEN
 
-#define randint(a,b) (rand()%(b-a)) - a
+#define randint(a,b) (rand()%(b-a)) - b
 
 // Single Object Functions
 
@@ -46,16 +48,12 @@ void calculate_gravity(struct object* b1, struct object* b2){
     double dist = sqrt(pow(xdif, 2) + pow(ydif, 2) + pow(zdif, 2));
     //printf("Distance : %f\n", (float)dist);
 
-    /*1. applying newtons law of universal gravitation  - doesn't really work
-    y1->vel.x -= (G * y1->mass * y2->mass * xdif) / pow(dist, 3);
-    y1->vel.y -= (G * y1->mass * y2->mass * ydif) / pow(dist, 3);
-    y1->vel.z -= (G * y1->mass * y2->mass * zdif) / pow(dist, 3);*/
-    //2. force(x/y/z) = force_m * cos(angle between (x/y/z) and force)
+    //force(x/y/z) = force_m * cos(angle between (x/y/z) and force)
     double force_m = (G * b1->mass * b2->mass) / pow(dist, 2);
     //printf("%d\n", force_m);
-    b1->vel.x -= (force_m * (xdif/force_m))*VEL_SCALE;
-    b1->vel.y -= (force_m * (ydif/force_m))*VEL_SCALE;
-    b1->vel.z -= (force_m * (zdif/force_m))*VEL_SCALE;
+    b1->vel.x -= (force_m * (xdif/force_m))*vel_scale;
+    b1->vel.y -= (force_m * (ydif/force_m))*vel_scale;
+    b1->vel.z -= (force_m * (zdif/force_m))*vel_scale;
     //printf("Velocity : %f, %f, %f\n", y1->vel.x, y1->vel.y, y1->vel.z);
 }
 
@@ -71,6 +69,9 @@ void update_object(struct object* b){
 
 void check_collision(struct object* b1, struct object* b2){
     if (CheckCollisionSpheres(b1->pos, b1->radius, b2->pos, b2->radius)){
+        //calculate the average density weighted by mass
+        b1->density = (b1->density*b1->mass + b2->density*b2->mass) / (b1->mass + b2->mass);
+
         //conserve mass
         b1->mass += b2->mass;
         b2->mass = 0;
@@ -87,11 +88,11 @@ void check_collision(struct object* b1, struct object* b2){
 
 // Multiple Object Functions
 
-struct object x[NUM_OBJECTS];
+struct object x[MAX_OBJECTS];
 void update_objects(){
-    for (int a = 0; a < NUM_OBJECTS; a++){
+    for (int a = 0; a < MAX_OBJECTS; a++){
         if (x[a].mass != 0) { update_object(&x[a]); }
-        for (int b = 0; b < NUM_OBJECTS; b++){
+        for (int b = 0; b < MAX_OBJECTS; b++){
             if (a != b && x[a].mass != 0 && x[b].mass != 0){
                 calculate_gravity(&x[a], &x[b]);
                 check_collision(&x[a], &x[b]);
@@ -111,6 +112,10 @@ void rotate_camera(Camera* camera){
     else if (IsKeyDown(KEY_DOWN)){ camera->position.y -= CAM_ROTATE_SPEED; }
     if (mouse.x > 0){ camera->position.z += CAM_ROTATE_SPEED*3; }
     else if (mouse.y < 0){ camera->position.z -= CAM_ROTATE_SPEED*3; }
+
+    //speed up or slow down the simulation
+    if (IsKeyDown(KEY_PERIOD) && vel_scale < 1){ vel_scale += VEL_CHANGE; }
+    else if (IsKeyDown(KEY_COMMA) && vel_scale > 0){ vel_scale -= VEL_CHANGE; }
 }
 
 // scenes 
@@ -119,7 +124,7 @@ char* scene_text;
 
 void scene_1(){
     scene_text = "Scene 1 : Two bodies orbiting a dense central mass";
-    for (int i = 0; i < NUM_OBJECTS; i++) {x[i].mass = 0;}
+    for (int i = 0; i < MAX_OBJECTS; i++) {x[i].mass = 0;}
     x[0].pos = (Vector3){0.0f, 0.0f, 0.0f};
     x[0].vel = (Vector3){0.0f, 0.0f, 0.0f};
     x[0].mass = pow(10, 10);
@@ -144,7 +149,7 @@ void scene_1(){
 
 void scene_2(){
     scene_text = "Scene 2 : Two bodies coliding into each other";
-    for (int i = 0; i < NUM_OBJECTS; i++) {x[i].mass = 0;}
+    for (int i = 0; i < MAX_OBJECTS; i++) {x[i].mass = 0;}
     x[0].pos = (Vector3){0.0f, 0.0f, 0.0f};
     x[0].vel = (Vector3){-0.0f, 0.0f, 0.0f};
     x[0].mass = 1000;
@@ -159,18 +164,43 @@ void scene_2(){
 }
 
 void scene_3(){
-    const int max_dist = 100;
+    const int max_dist = 75;
+    const int min_dist = -75;
+    const int num_objects = 50;
     const Color colours[5] = {RED, BLUE, GREEN, PURPLE, ORANGE};
     scene_text = "Scene 3 : Many objects clumping together";
 
-    for (int i = 0; i < NUM_OBJECTS; i++) {
-        x[i].pos = (Vector3){randint(0, max_dist), randint(0, max_dist), randint(0, max_dist)};
+    for (int i = 0; i < num_objects; i++) {
+        x[i].pos = (Vector3){randint(min_dist, max_dist), randint(min_dist, max_dist), randint(min_dist, max_dist)};
         x[i].vel = (Vector3){-0.0f, 0.0f, 0.0f};
-        x[i].mass = 10;
+        x[i].mass = randint(10, 100);
         x[i].density = 10;
         x[i].col = colours[rand()%5];
         calculate_radius(&x[i]);
     }
+}
+
+void scene_4(){
+    const int max_dist = 150;
+    const int min_dist = -150;
+    const int num_objects = 30;
+    const Color colours[5] = {RED, BLUE, GREEN, PURPLE, ORANGE};
+    scene_text = "Scene 4 : Ring of particles orbiting a central mass";
+
+    for (int i = 0; i < num_objects; i++) {
+        x[i].pos = (Vector3){randint(min_dist, max_dist), randint(min_dist, max_dist), randint(min_dist, max_dist)};
+        x[i].vel = (Vector3){randint(-1, 1)/10, randint(-1, 1)/10, randint(-1, 1)/10};
+        x[i].mass = randint(1, 10);
+        x[i].density = 1;
+        x[i].col = colours[rand()%5];
+        calculate_radius(&x[i]);
+    }
+
+    x[0].pos = (Vector3){0.0f, 0.0f, 0.0f};
+    x[0].vel = (Vector3){-0.0f, 0.0f, 0.0f};
+    x[0].mass = 1000;
+    x[0].density = 100;
+    calculate_radius(&x[0]);
 }
 
 void check_scene_change(){
@@ -180,7 +210,35 @@ void check_scene_change(){
         scene_2();
     } else if (IsKeyPressed(KEY_KP_3)){
         scene_3();
+    } else if (IsKeyPressed(KEY_KP_4)){
+        scene_4();
     }
+}
+
+Camera camera = { 0 };
+
+void draw_frame(){
+    char vel_text[30];
+    
+    BeginDrawing();
+
+    ClearBackground(BACKGROUND);
+    rotate_camera(&camera);
+    check_scene_change();
+
+    BeginMode3D(camera);
+    update_objects();
+    DrawGrid(50, 2.0f);
+    EndMode3D();
+
+    DrawFPS(50,50);
+    DrawText(scene_text, WIDTH/2, 50, TEXT_SCALE, TEXT_COLOUR);
+
+    sprintf(vel_text, "Velocity scale : %f", vel_scale);
+    DrawText(vel_text, 50, 100, TEXT_SCALE, TEXT_COLOUR);
+    DrawText("Controls :\n > / < to increase/decrease velocity\n WASD to move camera\n 12345 to select scene", 50, 150, TEXT_SCALE, TEXT_COLOUR);
+
+    EndDrawing();
 }
 
 int main(){
@@ -195,7 +253,6 @@ int main(){
     srand(t);
 
     //setup camera so it is looking down on the scene
-    Camera camera = { 0 };
     camera.position = (Vector3){ 30.0f, 30.0f, 30.0f }; // Camera position
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
@@ -203,24 +260,10 @@ int main(){
     camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
     scene_1();
-    
+
     //mainloop
     while (!WindowShouldClose()) {
-        BeginDrawing();
-
-        ClearBackground(BACKGROUND);
-        rotate_camera(&camera);
-        check_scene_change();
-
-        BeginMode3D(camera);
-        update_objects();
-        DrawGrid(30, 1.0f);
-        EndMode3D();
-
-        DrawFPS(50,50);
-        DrawText(scene_text, WIDTH/2, 50, 20, DARKGREEN);
-
-        EndDrawing();
+        draw_frame();
     }
     
     CloseWindow();
